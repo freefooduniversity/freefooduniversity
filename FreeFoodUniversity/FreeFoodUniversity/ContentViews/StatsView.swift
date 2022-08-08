46ad643a652e5522847090e8f246ddcc048eda36
William Sirotkin <williamsirotkin@gmail.com>
2022-08-08 11:04:02 -0400

    add to food_events stat when adding food

--- FreeFoodUniversity/FreeFoodUniversity/ContentViews/StatsView.swift
+++ FreeFoodUniversity/FreeFoodUniversity/ContentViews/StatsView.swift
@@ -38,3 +38,40 @@ struct StatsView: View {
         }
     }
 }
+
+func updateFoodEvents(college: String, food_events_change: Int) {
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/stats/food_events/update/" + college) else {
+        return
+    }
+   
+    var request = URLRequest(url: url)
+    
+    request.httpMethod = "PATCH"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    let body: [String: AnyHashable] = [
+        "food_events_change": food_events_change
+    ]
+     
+   request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+    
+    
+    // Make the Request
+    let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        guard let data = data, error == nil else {
+            return
+        }
+        
+        do {
+            let response = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            print("SUCESSS")
+            print(response)
+        } catch {
+            print("An error occured: ")
+            print(error)
+        }
+         
+    }
+    task.resume()
+    
+}
