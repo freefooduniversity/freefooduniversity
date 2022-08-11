1546fe35e996b12bed48bbaa15428f50a1318301
stephennemeth4 <83983441+stephennemeth@users.noreply.github.com>
2022-08-11 17:08:43 -0400

    Used a Picker for dropDown and moved into a seperate file

---
+++ FreeFoodUniversity/FreeFoodUniversity/ContentViews/SelectCollegeDropDownView.swift
@@ -0,0 +1,79 @@
+//
+//  SelectCollegeDropDownView.swift
+//  FreeFoodUniversity
+//
+//  Created by Stephen Nemeth on 8/11/22.
+//
+
+import SwiftUI
+import DropDown
+import UIKit
+
+
+struct SelectStateDropDownView : View {
+    @State private var selectedState = "GA"
+    let states = [
+        "AL",
+        "AK",
+        "AZ",
+        "AR",
+        "CA",
+        "CO",
+        "CT",
+        "DE",
+        "DC",
+        "FL",
+        "GA",
+        "HI",
+        "ID",
+        "IL",
+        "IN",
+        "IA",
+        "KS",
+        "KY",
+        "LA",
+        "ME",
+        "MD",
+        "MA",
+        "MI",
+        "MN",
+        "MS",
+        "MO",
+        "MT",
+        "NE",
+        "NV",
+        "NH",
+        "NJ",
+        "NM",
+        "NY",
+        "NC",
+        "ND",
+        "OH",
+        "OK",
+        "OR",
+        "PA",
+        "RI",
+        "SC",
+        "SD",
+        "TN",
+        "TX",
+        "UT",
+        "VT",
+        "VA",
+        "WA",
+        "WI",
+        "WY",
+    ]
+    
+    var body : some  View {
+        VStack {
+            Picker("Select Your State", selection: $selectedState) {
+                ForEach(states, id: \.self) {
+                    Text($0)
+                }
+                
+                Text("Your state is: \(selectedState)")
+            }
+        }
+    }
+}
