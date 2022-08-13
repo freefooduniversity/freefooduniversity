b7ef4dd4ec09e9c584450ce45fe5923bfff7d337
William Sirotkin <williamsirotkin@gmail.com>
2022-08-12 21:51:20 -0400

    can access camera roll from app. need to switch it to camera

---
+++ FreeFoodUniversity/FreeFoodUniversity/ImagePicker.swift
@@ -0,0 +1,26 @@
+//
+//  ImagePicker.swift
+//  FreeFoodUniversity
+//
+//  Created by William Sirotkin on 8/12/22.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        // leave alone
+    }
+}
