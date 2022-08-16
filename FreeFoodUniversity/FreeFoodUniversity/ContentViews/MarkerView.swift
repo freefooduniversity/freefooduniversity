da42bce82a4b88d1f19b9eef5409a8bdbb8b0100
William Sirotkin <williamsirotkin@gmail.com>
2022-08-16 14:26:41 -0400

    marker display ui improvement

--- FreeFoodUniversity/FreeFoodUniversity/ContentViews/MarkerView.swift
+++ FreeFoodUniversity/FreeFoodUniversity/ContentViews/MarkerView.swift
@@ -17,38 +17,46 @@ struct MarkerView: View {
     
     var body: some View {
         VStack {
-            Button(action: {
-                markerClicked = ""
-            }) {
                 HStack {
-                    Text("")
-                    Image("back")
-                    Text("Go Back      ")
-                        .font(.custom("Helvetica Neue", size: 16))
-                        .foregroundColor(.white)
+                    Text(makeMarkerTitle(food: markerData.food, building: "Creswell"))
+                        .bold()
+                        .font(.custom("Helvetica Neue", size: 20))
+                        .foregroundColor(.black)
+                        .position(x:180, y: 15)
+                    Button(action: {
+                        markerClicked = ""
+                    }) {
+                        HStack {
+                            Text("")
+                            Text("Back    ")
+                                .font(.custom("Helvetica Neue", size: 16))
+                                .foregroundColor(.white)
+                        }
+                    }.background(Color.purple).cornerRadius(15).position(x: 140, y: 25)
                 }
-            }.background(Color.purple).cornerRadius(15).position(x:70, y: 30)
-             
             VStack {
-                Text(makeMarkerTitle(food: markerData.food, building: "Creswell"))
-                    .bold()
-                    .font(.custom("Helvetica Neue", size: 20))
-                    .foregroundColor(.black)
                 HStack {
-                    VStack {
-                        Text("Event Lasts From : " + String(markerData.start_time) + " - " + String(markerData.end_time))
-                        Text("Signed Up: " + String(markerData.dibs) + " / " + String(markerData.capacity))
-                        Text("Event: None Yet")
-                        Text("Additional Info: Coming Soon")
-                    }
+                    VStack(alignment: .leading) {
+                        Text("Time: ").bold().underline()
+                        Text(String(markerData.start_time) + " - " + String(markerData.end_time))
+                        Text(" ")
+                        Text("Signed Up: ").bold().underline()
+                        Text(String(markerData.dibs) + " / " + String(markerData.capacity))
+                        Text(" ")
+                        Text("Event: ").bold().underline()
+                        Text("None Yet")
+                        Text(" ")
+                        Text("Coming Soon")
+                    }.position(x: 70, y: 50)
                     VStack {
                         Image("Headshot")
                             .frame(width: 50, height: 100)
-                    }
-                }
+                    }.position(x: 125, y: 55)
+                }.position(x: 180, y: 35)
             }
         }.position(x: 200, y: 95)
     }
 
 }
+
 
