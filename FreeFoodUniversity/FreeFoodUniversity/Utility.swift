c24be4befce5a1b2b948f4083ec5d0139e52d57c
William Sirotkin <williamsirotkin@gmail.com>
2022-08-19 12:02:28 -0400

    api secruity

--- FreeFoodUniversity/FreeFoodUniversity/Utility.swift
+++ FreeFoodUniversity/FreeFoodUniversity/Utility.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 var Markers: [Marker] = []
+let foo = Bundle.main.object(forInfoDictionaryKey: "food")as! String
 
 func getStats(completion: @escaping (Stats) -> (), college: String) {
-    guard let url = URL(string: "https://free-food-university.azurewebsites.net/stats/" + college) else {
+    print(Bundle.main.object)
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/" + foo + "/stats/" + college) else {
         return
     }
         
@@ -26,7 +28,7 @@ func getStats(completion: @escaping (Stats) -> (), college: String) {
 }
 
 func getAllMarkers(completion: @escaping ([Marker]) -> ()) {
-    guard let url = URL(string: "https://free-food-university.azurewebsites.net/marker/all") else {
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/" + foo + "/marker/all") else {
         return
     }
         
@@ -42,7 +44,7 @@ func getAllMarkers(completion: @escaping ([Marker]) -> ()) {
 }
 
 func getAllMarkersForCollege(completion: @escaping ([Marker]) -> (), college: String) {
-    guard let url = URL(string: "https://free-food-university.azurewebsites.net/marker/" + college) else {
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/" + foo + "/marker/" + college) else {
         return
     }
         
@@ -58,7 +60,7 @@ func getAllMarkersForCollege(completion: @escaping ([Marker]) -> (), college: St
 }
 
 func getMarkerFromTitleAndCollege(completion: @escaping ([Marker]) -> (), college: String, food: String) {
-    guard let url = URL(string: "https://free-food-university.azurewebsites.net/marker/" + college + "/" + food) else {
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/" + foo + "/marker/" + college + "/" + food) else {
         return
     }
         
@@ -93,7 +95,7 @@ func getMarkerFromTitleAndCollege(completion: @escaping ([Marker]) -> (), colleg
     food = String(title.prefix(upTo: index!))
     building = String(title.substring(from: fromIndex))
 
-    guard let url = URL(string: "https://free-food-university.azurewebsites.net/marker/title/college/" + food + "/" + building + "/" + college) else {
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/" + foo + "/marker/title/college/" + food + "/" + building + "/" + college) else {
         return
     }
         
@@ -150,7 +152,7 @@ func makeMarkerTitle(food: String, building: String)  -> String{
 
 
 func updateMarkerButton(id: Int, button: String, college: String) {
-    guard let url = URL(string: "https://free-food-university.azurewebsites.net/marker/button/" + String(id) + "/" + button + "/" + college) else {
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/" + foo + "/marker/button/" + String(id) + "/" + button + "/" + college) else {
         return
     }
     
@@ -161,7 +163,7 @@ func updateMarkerButton(id: Int, button: String, college: String) {
 }
 
 func deleteFedToday() {
-    guard let url = URL(string: "https://free-food-university.azurewebsites.net/stats/fed_today/reset") else {
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/" + foo + "/stats/fed_today/reset") else {
         return
     }
     
@@ -169,3 +171,196 @@ func deleteFedToday() {
      //   let stats = try!JSONDecoder().decode([String].self, from: data!)
     }.resume()
 }
+
+func addMarker(id: Int, foodSelection: String, lat: Double, long: Double, college: String, duration : String, capacity: Int, building: String, event: String, additional_info: String, email: String) {
+    getTimeZone()
+    if (lat == 37.0902 || long == -95.7129) {
+        print("TURN LOCATION ON")
+        return
+    }
+    
+    if (emailSet.contains(email) && email != "6B9D47FA-DF0A-4598-9E55-51550A3F4974") {
+        print("You've already placed a marker")
+        return
+    }
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/" + foo + "/marker/add") else {
+        return
+    }
+   
+    var request = URLRequest(url: url)
+    
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    var food = getFoodFromDropDownName(food: foodSelection)
+    let body: [String: AnyHashable] = [
+        "id": id,
+        "food": food,
+        "lat": lat,
+        "long": long,
+        "college": college,
+        "start_time": getStartTime(),
+        "end_time": getEndTime(duration: duration),
+        "time_zone": getTimeZone(),
+        "capacity": capacity,
+        "dibs": 0,
+        "likes": 0,
+        "dislikes": 0,
+        "reports": 0,
+        "creator_email": email,
+        "pic_url": "place_holder",
+        "event": event,
+        "building": building,
+        "additional_info": additional_info
+    ]
+     
+   request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+    
+    
+    // Make the Request
+    let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        guard let data = data, error == nil else {
+            return
+        }
+        
+        do {
+            let response = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            print("SUCESSS")
+            print(response)
+        } catch {
+            print("An error occured: ")
+            print(error)
+        }
+         
+    }
+    task.resume()
+    
+}
+
+func getStatsForState(completion: @escaping (Stats) -> (), state: String) {
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/" + foo + "/stats/state") else {
+        return
+    }
+   
+    var request = URLRequest(url: url)
+    
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    let body: [String: AnyHashable] = [
+        "colleges": getCollegesByState(selectedState: state)
+    ]
+     
+   request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+    
+    
+    // Make the Request
+    /*
+    let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        guard let data = data, error == nil else {
+            return
+        }
+        
+        do {
+            let response = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            print("SUCESSS")
+            print(response)
+        } catch {
+            print("An error occured: ")
+            print(error)
+        }
+         
+    }
+    task.resume()
+     */
+    URLSession.shared.dataTask(with: request) { (data, _, _) in
+        let stats = try!JSONDecoder().decode(Stats.self, from: data!)
+        DispatchQueue.main.async {
+            completion(stats)
+        }
+    }.resume()
+    
+}
+
+func getMarkersForState(completion: @escaping ([Marker]) -> (), state: String) {
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/" + foo + "/marker/state") else {
+        return
+    }
+   
+    var request = URLRequest(url: url)
+    
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    let body: [String: AnyHashable] = [
+        "colleges": getCollegesByState(selectedState: state)
+    ]
+     
+   request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+    
+    
+    // Make the Request
+    /*
+    let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        guard let data = data, error == nil else {
+            return
+        }
+        
+        do {
+            let response = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            print("SUCESSS")
+            print(response)
+        } catch {
+            print("An error occured: ")
+            print(error)
+        }
+         
+    }
+    task.resume()
+     */
+    URLSession.shared.dataTask(with: request) { (data, _, _) in
+        let markers = try!JSONDecoder().decode([Marker].self, from: data!)
+        DispatchQueue.main.async {
+            completion(markers)
+        }
+    }.resume()
+    
+}
+
+func updateFoodEvents(college: String, food_events: Int) {
+    guard let url = URL(string: "https://free-food-university.azurewebsites.net/" + foo + "/stats/food_events/update/" + college) else {
+        return
+    }
+   
+    var request = URLRequest(url: url)
+    
+    request.httpMethod = "PATCH"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    let body: [String: AnyHashable] = [
+        "food_events": food_events
+    ]
+     
+   request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+    
+    
+    // Make the Request
+    let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        guard let data = data, error == nil else {
+            return
+        }
+        
+        do {
+            let response = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            print("SUCESSS")
+            print(response)
+        } catch {
+            print("An error occured: ")
+            print(error)
+        }
+         
+    }
+    task.resume()
+    
+}
+ 
